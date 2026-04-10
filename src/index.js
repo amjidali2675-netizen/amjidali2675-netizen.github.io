54b9eae6158232b1bf38f3fafd01138663594947
PlayLunarmc <amjidali2675@gmail.com>
2026-04-10 19:29:27 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+@import url('https://fonts.googleapis.com/css2?family=Unbounded:wght@400;700;900&family=Outfit:wght@300;400;500;600&display=swap');
+
+body {
+  margin: 0;
+  background: #05050A;
+  color: white;
+  font-family: 'Outfit', sans-serif;
+}
+
+h1, h2 {
+  font-family: 'Unbounded', sans-serif;
+}
